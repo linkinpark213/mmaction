7d1618328eb0931a99383bd0f33db18bd061d5af
linkinpark213 <linkinpark213@outlook.com>
2019-10-25 17:23:55 +0800

    Debugging for demo #10

--- tools/demo_recognizer.py
+++ tools/demo_recognizer.py
@@ -1,5 +1,6 @@
 import argparse
-
+import cv2
+import numpy as np
 import torch
 import mmcv
 from mmcv.runner import load_checkpoint, parallel_test, obj_from_dict
@@ -41,6 +42,7 @@ def _data_func(data, device_id):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Test an action recognizer')
+    parser.add_argument('video_path', help='path to test video')
     parser.add_argument('config', help='test config file path')
     parser.add_argument('checkpoint', help='checkpoint file')
     parser.add_argument(
@@ -50,7 +52,6 @@ def parse_args():
         default=1,
         type=int,
         help='Number of processes per GPU')
-    parser.add_argument('--out', help='output result file')
     parser.add_argument('--use_softmax', action='store_true',
                         help='whether to use softmax score')
     args = parser.parse_args()
@@ -60,8 +61,24 @@ def parse_args():
 def main():
     args = parse_args()
 
-    if args.out is not None and not args.out.endswith(('.pkl', '.pickle')):
-        raise ValueError('The output file must be a pkl file.')
+    cap = cv2.VideoCapture(args.video_path)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    buf = np.empty((frame_count, frame_height, frame_width, 3), np.dtype('uint8'))
+
+    fc = 0
+    ret = True
+
+    while (fc < frame_count and ret):
+        ret, buf[fc] = cap.read()
+        fc += 1
+
+    cap.release()
+
+    buf = buf.astype(np.int8)
+    buf = buf.transpose((0, 3, 1, 2))
+    buf = np.expand_dims(buf, 0)
 
     cfg = mmcv.Config.fromfile(args.config)
     # set cudnn_benchmark
@@ -72,56 +89,13 @@ def main():
     if cfg.data.test.oversample == 'three_crop':
         cfg.model.spatial_temporal_module.spatial_size = 8
 
-    dataset = obj_from_dict(cfg.data.test, datasets, dict(test_mode=True))
-    if args.gpus == 1:
-        model = build_recognizer(
-            cfg.model, train_cfg=None, test_cfg=cfg.test_cfg)
-        load_checkpoint(model, args.checkpoint, strict=True)
-        model = MMDataParallel(model, device_ids=[0])
+    model = build_recognizer(
+        cfg.model, train_cfg=None, test_cfg=cfg.test_cfg)
+    load_checkpoint(model, args.checkpoint, strict=True)
 
-        data_loader = build_dataloader(
-            dataset,
-            imgs_per_gpu=1,
-            workers_per_gpu=cfg.data.workers_per_gpu,
-            num_gpus=1,
-            dist=False,
-            shuffle=False)
-        outputs = single_test(model, data_loader)
-    else:
-        model_args = cfg.model.copy()
-        model_args.update(train_cfg=None, test_cfg=cfg.test_cfg)
-        model_type = getattr(recognizers, model_args.pop('type'))
-        outputs = parallel_test(
-            model_type,
-            model_args,
-            args.checkpoint,
-            dataset,
-            _data_func,
-            range(args.gpus),
-            workers_per_gpu=args.proc_per_gpu)
+    outputs = inference(model, buf)
 
-    if args.out:
-        print('writing results to {}'.format(args.out))
-        mmcv.dump(outputs, args.out)
-
-    gt_labels = []
-    for i in range(len(dataset)):
-        ann = dataset.get_ann_info(i)
-        gt_labels.append(ann['label'])
-
-    if args.use_softmax:
-        print("Averaging score over {} clips with softmax".format(
-            outputs[0].shape[0]))
-        results = [softmax(res, dim=1).mean(axis=0) for res in outputs]
-    else:
-        print("Averaging score over {} clips without softmax (ie, raw)".format(
-            outputs[0].shape[0]))
-        results = [res.mean(axis=0) for res in outputs]
-    top1, top5 = top_k_accuracy(results, gt_labels, k=(1, 5))
-    mean_acc = mean_class_accuracy(results, gt_labels)
-    print("Mean Class Accuracy = {:.02f}".format(mean_acc * 100))
-    print("Top-1 Accuracy = {:.02f}".format(top1 * 100))
-    print("Top-5 Accuracy = {:.02f}".format(top5 * 100))
+    print(outputs)
 
 
 if __name__ == '__main__':
